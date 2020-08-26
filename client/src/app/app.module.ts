b86a5d02c51d5491f8bb8dc1a13317f7d31b9fef
Samba <ksamba@innoclique.com>
2020-08-27 01:01:16 +0530

    navigation setup

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,7 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import {SharedModule} from './shared/shared.module'
+import { SecondComponent } from './shared/second/second.component';
+import { FirstComponent } from './shared/first/first.component';
+import { Routes, RouterModule, ROUTES } from '@angular/router';
+const appRoutes: Routes = [
+  { path: '', component: AppComponent, data: { title: 'First Component' } },
+  { path: 'first', component: FirstComponent, data: { title: 'First Component' } },
+  { path: 'second', component: SecondComponent, data: { title: 'Second Component' } }
+];
 @NgModule({
   declarations: [
     AppComponent
@@ -12,7 +20,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    SharedModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ client/src/app/shared/navbar/navbar.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit,ViewChild } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent implements OnInit {
+  @ViewChild('drawer', { static: false }) 
+  drawer: MatSidenav;
+  constructor() { }
+  opened: boolean;
+  ngOnInit(): void {
+  }
+
+}
